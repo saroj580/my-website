1cfb447fe87b7e5ec4c4deed7d73c334ecc9ff14
saroj580 <chaudharysaroj819@gmail.com>
2024-02-29 23:05:26 +0545

    My first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let typed = new Typed(".text", {
+    strings:["Frontend Developer", "Youtuber", "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    // startDelay:500,
+    loop:true,
+
+});
+
